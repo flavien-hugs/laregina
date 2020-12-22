b83db235e4b515c9611060c7f01146e8f30d0d84
flavien-hugs <geekactux@gmail.com>
2020-12-22 16:40:54 +0000

    add template 404, 403, 405 and update home page

--- core/urls.py
+++ core/urls.py
@@ -7,12 +7,42 @@ The `urlpatterns` list routes URLs to views. For more information
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.shortcuts import render
+from django.urls import path, include
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 admin.autodiscover()
 
+def handler404(request, exception, template_name='404.html'):
+    return render(request, template_name=template_name, status=404,
+        context={'page_title': 'Page non trouvée - Erreur 404'})
+
+def handler403(request, exception, template_name='403.html'):
+    return render(request, template_name=template_name, status=403,
+        context={'page_title': 'Page non trouvée - Erreur 404'})
+
+
+def handler500(request, template_name='500.html'):
+    return render(request, template_name=template_name,
+        status=500, context={'page_title': 'Erreur interne'})
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
 ]
+
+handler404 = handler404
+handler403 = handler403
+handler201600 = handler500
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += [
+        path('404', handler404, {'exception': Exception()}),
+        path('403', handler403, {'exception': Exception()}),
+        path('500', handler500),
+    ]
