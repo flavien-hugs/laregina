a369ec089d6ebc0afd781264825aaff4e3824708
flavien-hugs <geekactux@gmail.com>
2020-12-23 04:25:47 +0000

    update template : add detail product template, liste categorie

--- core/pages/urls.py
+++ core/pages/urls.py
@@ -6,9 +6,20 @@ from django.views.decorators.cache import cache_page
 
 
 urlpatterns  = [
-path('account/seller/register/', TemplateView.as_view(
+    path('account/seller/register/', TemplateView.as_view(
         extra_context={'page_title': 'Créer un compte', 'page_description': "Ouvrir ma boutique"
         }, template_name='accounts/register.html'), name='register'),
+    path('account/seller/connexion/', TemplateView.as_view(
+        extra_context={'page_title': 'Se connecter', 'page_description': "Connectez-vous à votre compte"
+        }, template_name='accounts/login.html'), name='login'),
+
+    path('list/', TemplateView.as_view(
+        extra_context={'page_title': 'Liste des produits', 'page_description': "Tous les produits des magasins"
+        }, template_name='catalogue/list.html'), name='list'),
+    path('detail/', TemplateView.as_view(
+        extra_context={'page_title': 'Detail du produit', 'page_description': "Detail du produit"
+        }, template_name='catalogue/detail.html'), name='detail'),
+
     path('about-us/', cache_page(60 * 2)(TemplateView.as_view(
         extra_context={'page_title': 'À propos de nous', 'page_description': "Qui sommes-nous ?"
         }, template_name='pages/about-us.html')), name='about-us'),
