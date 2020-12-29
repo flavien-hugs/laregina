65631ed15b0601c16cc7cac2f06c073d42c68b5e
flavien-hugs <geekactux@gmail.com>
2020-12-29 12:39:14 +0000

    update seller dashboard

---
+++ core/templatetags/apptags.py
@@ -0,0 +1,12 @@
+# app tags
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def active_nav(context, name):
+    if context['request'].resolver_match.url_name == name:
+        return 'active'
+    return ''
