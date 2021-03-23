41be168225d0e01bfb1cfed385a062574575b2af
flavien-hugs <geekactux@gmail.com>
2021-03-23 10:33:18 +0000

    update catalogue url

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from catalogue.views import ProductListView, show_product
+from catalogue import views
+
 
 app_name = 'catalogue'
 urlpatterns = [
-    path('tous-les-produits/', ProductListView.as_view(), name='product_list'),
-    path('detail/<slug>/', show_product, name='product_detail'),
+    path('tous-les-produits/', views.ProductListView.as_view(), name='product_list'),
+    path('detail/<slug>/', views.show_product, name='product_detail'),
 ]
