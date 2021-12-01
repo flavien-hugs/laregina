bc4e03bf1959a31c741a998cf6cc1231aadcf9ec
flavien-hugs <flavienhugs@pm.me>
2021-12-01 17:38:03 +0000

    update category views

--- category/views.py
+++ category/views.py
@@ -1,24 +1,25 @@
 # category.views.py
 
-from django.views.generic import DetailView
+from django.views import generic
 
+from analytics import utils
 from category.models import Category
 from catalogue.models import Product
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(generic.DetailView):
     model = Category
+    paginate_by = 20
     template_name = "category/category_detail.html"
+
+    def get_queryset(self):
+        return Category.objects.all()
 
     def get_context_data(self, **kwargs):
         obj = self.get_object().get_descendants(include_self=True)
         kwargs['object_list'] = Product.objects.filter(category__in=obj)
         kwargs['category'] = self.model.objects.all()
+        kwargs['product_recommended'] = utils.get_recently_viewed(self.request)
         kwargs['page_title'] = 'Catégories: {category_name}'.format(
             category_name=self.object.name)
         return super().get_context_data(**kwargs)
-
-    def get_queryset(self):
-        from django.db import connection
-        queries = connection.queries
-        return Category.objects.all()
