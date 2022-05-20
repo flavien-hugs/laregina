f7ef00051ad172bb2fa10133ff7a105253effb22
flavien-hugs <geekactux@gmail.com>
2022-05-20 04:12:46 +0000

    update reviews apps models

--- reviews/models.py
+++ reviews/models.py
@@ -9,10 +9,6 @@ from reviews.managers import ActiveProductReviewManager
 
 
 class ProductReview(models.Model):
-    """
-    classe de modèle contenant des données
-    de révision de produit associées à une instance de produit
-    """
     
     RATINGS = ((1,1), (2,2), (3,3), (4,4), (5,5),)
 
@@ -34,8 +30,8 @@ class ProductReview(models.Model):
         choices=RATINGS
     )
     content = models.TextField(verbose_name='avis client')
-    created_time_at = models.DateField(auto_now_add=True)
-    created_hour_at = models.TimeField(auto_now_add=True)
+    created_time_at = models.DateField(editable=False, auto_now_add=True)
+    created_hour_at = models.TimeField(editable=False, auto_now_add=True)
     is_approved = models.BooleanField(
         verbose_name='approuvé ?',
         default=False
@@ -45,15 +41,10 @@ class ProductReview(models.Model):
     approved = ActiveProductReviewManager()
 
     class Meta:
-        db_table = 'reviews_db'
-        unique_together = ['product']
-        index_together = (('product',),)
         ordering = ['-rating', '-created_time_at', '-created_time_at']
         get_latest_by = ['-created_time_at', '-created_time_at', '-rating']
         verbose_name_plural = 'feedbacks'
-        indexes = [
-            models.Index(fields=['id'], name='id_index_reviews'),
-        ]
+        indexes = [models.Index(fields=['id'],),]
 
     def __str__(self):
         return f'{self.name} a donné son avis sur {self.product.name}: {self.rating} le {self.created_time_at}'
@@ -65,9 +56,5 @@ class ProductReview(models.Model):
     def cache_key(self):
         return self.get_absolute_url()
 
-# attacher des signaux aux classes de modèles de review
-# pour mettre à jour les données du cache sur les opérations
-# de sauvegarde et de suppression
-
 models.signals.post_save.connect(cache_update, sender=ProductReview)
-models.signals.post_delete.connect(cache_evict, sender=ProductReview) 
+models.signals.post_delete.connect(cache_evict, sender=ProductReview)
