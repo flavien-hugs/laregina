1b81d37e58e5d77c420c109fb4f43c2064d802b9
flavien-hugs <geekactux@gmail.com>
2022-07-26 15:05:37 +0000

    improve catalogue views

--- catalogue/views.py
+++ catalogue/views.py
@@ -18,22 +18,31 @@ from cart import cart
 from analytics import utils
 from category.models import Category
 
-from pages.models import Promotion
 from catalogue.models import Product
 from pages.mixins import PromotionMixin
 from reviews.models import ProductReview
 from catalogue.filters import FilterMixin
 from reviews.forms import ProductReviewForm
+from pages.models import Promotion, HomePage
 from catalogue.forms import ProductAddToCartForm
 
 
-class HomeView(PromotionMixin, generic.TemplateView):
+class TemplateChoises:
+    if HomePage.objects.filter(page=0):
+        template_name = "index.html"
+    elif HomePage.objects.filter(page=1):
+        template_name = "market.html"
+    elif HomePage.objects.filter(page=2):
+        template_name = "combine.html"
+    else:
+        template_name = "index.html"
 
-    template_name = 'index.html'
+
+class HomeView(TemplateChoises, PromotionMixin, generic.TemplateView):
+
     queryset = Category.objects.all()
 
     def get_context_data(self, **kwargs):
-
         kwargs['promotions'] = self.promotions()
         kwargs['destockages'] = self.get_destockages()
         kwargs['sales_flash'] = self.get_sales_flash()
@@ -46,6 +55,10 @@ class HomeView(PromotionMixin, generic.TemplateView):
 
         supermarket = self.queryset.get(pk=7).get_descendants(include_self=True)
         kwargs['supermarkets'] = Product.objects.filter(category__in=supermarket)[:15]
+
+        supermarkets = self.queryset.get(pk=22).get_descendants(include_self=True)
+        kwargs['categories'] = supermarkets.get_ancestors(include_self=False)
+        kwargs['products'] = Product.objects.filter(category__in=supermarkets)[:15]
 
         return super(HomeView, self).get_context_data(**kwargs)
 
