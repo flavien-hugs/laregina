0a8cf23e6b55fe895b87af3b66b8f7d72ee357fe
flavien-hugs <geekactux@gmail.com>
2022-09-02 08:43:30 +0000

    edit category admin

--- category/admin.py
+++ category/admin.py
@@ -11,16 +11,15 @@ from mptt.admin import DraggableMPTTAdmin
 @admin.register(Category)
 class CategoryAdmin(DraggableMPTTAdmin, admin.ModelAdmin):
     model = Category
-    list_per_page = 10
     mptt_level_indent = 10
     mptt_indent_field = "parent"
     expand_tree_by_default = False
-    
+
     date_hierarchy = 'created_at'
-    
+
     fieldsets = (
         ('catégorie', {'fields':
-            (   
+            (
                 "parent", "name",
                 "image", "is_active",
             )}
