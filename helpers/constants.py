45fd50c57d83bd383446baf3bd549446c5204700
flavien-hugs <geekactux@gmail.com>
2022-10-29 13:23:21 +0000

    edit constant value country

---
+++ helpers/constants.py
@@ -0,0 +1,19 @@
+CIV = "Côte d'Ivoire"
+SEN = "Sénégal"
+TOG = "Togo"
+BF = "Burkina Faso"
+CMR = "Cameroun"
+RDC = "Congo RDC CDF"
+GUI = "Guinée"
+CON = "Congo RDC USD"
+NIG = "Niger"
+BEN = "Benin"
+MAL = "Mali"
+COM = "Comores"
+
+COUNTRY_CHOICES = (
+    (None, 'Veuillez sélectionner un pays'),
+    (CIV, "Côte d'Ivoire")
+)
+
+DEFAULT_COUNTRY_CHOICES = CIV
