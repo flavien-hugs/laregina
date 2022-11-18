b2a2c8202196725d2c72657933b6acc06f39c2dd
flavien-hugs <geekactux@gmail.com>
2022-11-18 22:53:49 +0000

    Add packages celery

--- checkout/checkout.py
+++ checkout/checkout.py
@@ -1,6 +1,7 @@
 # checkout.checkout.py
 
 import requests
+import threading
 
 from django.urls import reverse
 from django.conf import settings
@@ -45,21 +46,14 @@ def create_order(request):
     order.save()
 
     if order.pk:
-
-        """si la sauvegarde de la commande a réussi"""
         cart_items = cart.get_cart_items(request)
 
         for cart_item in cart_items:
-            """
-            créer un article de commande pour chaque article du panier
-            """
             order_item = OrderItem()
             order_item.order = order
             order_item.quantity = cart_item.quantity
             order_item.product = cart_item.product
             order_item.save()
-
-        # tout est prêt, videz le chariot
         cart.empty_cart(request)
     return order
 
